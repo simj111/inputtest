6a3324a16a921e6687ab6390b96e3dc207525c0e
simj111 <simj1_11@uni.worc.ac.uk>
2013-11-27 12:25:42 +0000

    input form

---
+++ submit.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function submitInfo()
+{
+	var userName = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var phoneNo = document.getElementById('tel').value;
+	var dateOfBirth = document.getElementById('dob').value;
+	var password = document.getElementById('pass').value;
+	alert (userName);
+}
